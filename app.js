914f28054fc519d77b6d4033d22949d7ceb12d40
miduzack <miduzack001@gmail.com>
2024-10-15 13:03:06 +0530

    check it

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express();
-const port = 4000;
+const port = 3000;
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/Crud-Database', { useNewUrlParser: true, useUnifiedTopology: true });
